d7d1fc8bca2f474311e1ef94d99f51efa336c7a4
Sirx <bradx2713@gmail.com>
2024-02-27 13:49:44 +0200

    Initial Commit

---
+++ Crazy_8_Cards_D5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crazy_8_Cards_D5
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crazy_8_Cards_D5/Crazy_8_Cards_D5App.swift
@@ -0,0 +1,17 @@
+//
+//  Crazy_8_Cards_D5App.swift
+//  Crazy_8_Cards_D5
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/27.
+//
+
+import SwiftUI
+
+@main
+struct Crazy_8_Cards_D5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
